67c02eace074cd683305261768ec7d8e64c3ff7e
Kketrin911 <katrin4ik911@gmail.com>
2024-07-26 17:31:09 +0300

    zadachi
    zadachi

---
+++ chisla.py
@@ -0,0 +1,15 @@
+a = int(input('Введите число a: '))
+b = int(input('Введите число b: '))
+if a>b:
+    print ('a>b')
+elif a<b:
+    print ('a<b')
+elif a==b:
+    print ('a=b')        
+print (a+b)
+print (a-b)
+print (a*b)
+if b!=0:
+    print (a/b)
+else:
+    print('На ноль делить нельзя')    
